479c749640803a701e78bd711bf524c33239f344
Tatsapat Saerejittima <sabaiprimo@gmail.com>
2021-03-11 15:01:06 +0200

    Add song player and update time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react'
+// Import Styles
+import './styles/app.scss'
+// Adding Components
+import Player from './components/Player'
+import Song from './components/Song'
+// Import Util
+import data from './utils'
+
 function App() {
+  // State
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   )
 }
